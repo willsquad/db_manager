d52b18a67bd17de4dd6d4b5a028af2e0d2a8951d
Ashil John <willsquad.aj@gmail.com>
2018-05-18 15:28:21 +0530

    DB Manager RHS Top-Bar Added

--- files/js/main.js
+++ files/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
    
+    /* RHS Navigation */
+    $('.dbmanager_rhs__top_bar__navigation__element').on('click', function(){
+        var self = $(this);
+        $('.dbmanager_rhs__top_bar__navigation__element').removeClass('active');
+        self.addClass('active');
+    });
+
 });
