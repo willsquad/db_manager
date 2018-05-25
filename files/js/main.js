6598b1ccd05431feda65cac8fbe0ce1e209e9476
Ashil John <willsquad.aj@gmail.com>
2018-05-25 15:20:14 +0530

    Sliding Nav added + Table Details Page's mobile version added

--- files/js/main.js
+++ files/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
     /* LHS Navigation */
-    $('.dbmanager_lhs__db_section__show_dbs__a_element').on('click', function(){
+    $('.dbmanager_lhs__db_section__show_dbs__a_element, .dbmanager_menu__db_section__show_dbs__a_element').on('click', function(){
         var self = $(this);
         var menu_element = self.attr('data-db');
         
-        $('.dbmanager_lhs__db_section__show_dbs__a_element').removeClass('active');
+        $('.dbmanager_lhs__db_section__show_dbs__a_element, .dbmanager_menu__db_section__show_dbs__a_element').removeClass('active');
         self.addClass('active');
 
         $('#top_nav_db_name').html(menu_element);
@@ -28,13 +28,13 @@ $(document).ready(function() {
     $(document).on('click', '#create_database', function(){
         $('.prompt_overlay').show();
         $('.prompt_add_database_div').show();
-        $('body').addClass('overlay_applied');
+        $('.dbmanager_rhs').addClass('overlay_applied');
     });
 
     $(document).on('click', '.cancel_database_delete_js, .cancel_database_modal_js', function() {
         $('.prompt_overlay').hide();
         $('.prompt_add_database_div').hide();
-        $('body').removeClass('overlay_applied');
+        $('.dbmanager_rhs').removeClass('overlay_applied');
     });
     /* Modal */
 
@@ -45,14 +45,14 @@ $(document).ready(function() {
 
     //".checkbox" change 
     $('.checkbox').change(function(){ 
-    //uncheck "select all", if one of the listed checkbox item is unchecked
-    if(false == $(this).prop("checked")){ //if this item is unchecked
-        $("#select_all").prop('checked', false); //change "select all" checked status to false
-    }
-    //check "select all" if all checkbox items are checked
-    if ($('.checkbox:checked').length == $('.checkbox').length ){
-        $("#select_all").prop('checked', true);
-    }
+        //uncheck "select all", if one of the listed checkbox item is unchecked
+        if(false == $(this).prop("checked")){ //if this item is unchecked
+            $("#select_all").prop('checked', false); //change "select all" checked status to false
+        }
+        //check "select all" if all checkbox items are checked
+        if ($('.checkbox:checked').length == $('.checkbox').length ){
+            $("#select_all").prop('checked', true);
+        }
     });
     /* Select all Checkboxes */
 
@@ -65,8 +65,32 @@ $(document).ready(function() {
     } else {
         $('#top_nav_table_name').html('table_1');
     }
-
-    
     /* URL Parameter */
+
+    /* Menu Button Click */
+    $('.menu_button').on('click', function(){
+        $('.dbmanager_menu').show();
+        $('.prompt_overlay').show();
+        $('.dbmanager_rhs').addClass('overlay_applied');
+    });
+    /* Menu Button Click */
+
+    /* Menu Close Click */
+    $('.dbmanager_menu__top_bar__close').on('click', function(){
+        $('.dbmanager_menu').hide();
+        $('.prompt_overlay').hide();
+        $('.dbmanager_rhs').removeClass('overlay_applied');
+    });
+    /* Menu Close Click */
+
+    /* When prompt overlay is clicked in mobile screens */
+    if ($(window).width() <= 767) {
+        $('.prompt_overlay').on('click', function(){
+            $('.dbmanager_menu').hide();
+            $('.prompt_overlay').hide();
+            $('.dbmanager_rhs').removeClass('overlay_applied');
+        });
+    }
+    /* When prompt overlay is clicked in mobile screens */
 
 });
